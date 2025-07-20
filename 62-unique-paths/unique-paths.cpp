18d279bc5f57be47b63583e5abb94451c866b886
Prathamesh Shetty <145214206+prathameshshetty@users.noreply.github.com>
2025-07-20 05:57:29 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (75.68%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
-
-
-    int f(int i,int j,vector<vector<int>> &dp)
-    {
-if(i<0 || j<0) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        
-        if(i==0 && j==0) return 1;
-
-        int up=f(i-1,j,dp);
-        int down=f(i,j-1,dp);
-
-        return dp[i][j]=up+down;
-    }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        return f(m-1,n-1,dp);
+        vector<int> prev(n,1);
+
+        for(int i=1;i<m;i++)
+        {
+            vector<int> temp(n,0);
+
+            for(int j=0;j<n;j++)
+            {
+                int up=0,left=0;
+                //No need for an condiution since i can get up in evrytcase
+                up=prev[j];
+                if(j>0) left=temp[j-1];
+
+                temp[j]=up+left;
+            }
+
+            prev=temp;
+        }
+
+        return prev[n-1];
     }
 };
