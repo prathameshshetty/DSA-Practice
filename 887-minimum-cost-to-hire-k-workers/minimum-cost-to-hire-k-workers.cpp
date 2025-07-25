d394833c9ce67ed98d8084a820c31d00f990db8f
Prathamesh Shetty <145214206+prathameshshetty@users.noreply.github.com>
2025-07-25 12:12:35 +0530

    Time: 18 ms (22.24%) | Memory: 32 MB (24.39%) - LeetSync

--- 887-minimum-cost-to-hire-k-workers/minimum-cost-to-hire-k-workers.cpp
+++ 887-minimum-cost-to-hire-k-workers/minimum-cost-to-hire-k-workers.cpp
@@ -1,47 +1,49 @@
 class Solution {
 public:
     double mincostToHireWorkers(vector<int>& quality, vector<int>& wage, int k) {
-        ios_base::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
-
+        int n=quality.size();
         vector<pair<double,int>> ratio;
 
-        int n=quality.size();
 
         for(int i=0;i<n;i++)
         {
             ratio.push_back({(double)wage[i]/quality[i],i});
         }
 
-        //now sort it
+        //now i sort them based on thier ratio
         sort(ratio.begin(),ratio.end());
 
-        //use a priority queuw
+        //The main thing is cost=minRatioPossibleInK * totalqualityOfKgroup
+        //Inorder to make the cost less, i got try making both of them less
+        //First I try decreasing the minRationINKPossible
+        //Then I try the totalQUalityless by replacing it with current K group
         priority_queue<int> maxHeap;
-        int qualitySum=0;
-        double maxRate=0.0;
-
+        int totalQuality=0;
+        double maxRatio=0;
         for(int i=0;i<k;i++)
         {
-            qualitySum+=quality[ratio[i].second];
-            maxRate=max(maxRate,ratio[i].first);
+            totalQuality+=quality[ratio[i].second];
+            maxRatio=max(maxRatio,ratio[i].first);
             maxHeap.push(quality[ratio[i].second]);
+
         }
 
-        double res=maxRate*qualitySum;
+        double res=maxRatio*totalQuality;
 
+        //now try by using raming k 
         for(int i=k;i<n;i++)
         {
-            maxRate=max(maxRate,ratio[i].first);
-            qualitySum-=maxHeap.top();
+            maxRatio=max(maxRatio,ratio[i].first);
+            totalQuality-=maxHeap.top();
             maxHeap.pop();
-
-            qualitySum+=quality[ratio[i].second];
+            totalQuality+=quality[ratio[i].second];
             maxHeap.push(quality[ratio[i].second]);
-            res=min(res,maxRate*qualitySum);
+
+            res=min(res,totalQuality*maxRatio);
         }
 
         return res;
+
+        //Now i got the least ratio possibt
     }
 };
