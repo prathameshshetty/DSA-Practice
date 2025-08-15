b4515af01a050ffcd81a56a155adca2cdaaaddc5
Prathamesh Shetty <145214206+prathameshshetty@users.noreply.github.com>
2025-08-15 12:11:31 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (96.21%) - LeetSync

--- 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.cpp
+++ 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.cpp
@@ -1,36 +1,15 @@
 class Solution {
 public:
     string largestGoodInteger(string num) {
-        //I'll put a unordered map
-        //along with a sliding widnow
-        unordered_map<char,int> mpp;
-    int n=num.size();
-for(int i=0;i<3;i++) mpp[num[i]]++;
-string ans="";
-int res=-1;
-if(mpp.size()==1) 
-{
-    res=num[0]-'0';
-}
-        for(int i=3;i<n;i++)
+        string ans="";
+        int n=num.size();
+        for(int i=0;i<n-2;i++)
         {
-            //remove then add
-            mpp[num[i-3]]--;
-            if(mpp[num[i-3]]<=0) mpp.erase(num[i-3]);
-
-            //now add
-            mpp[num[i]]++;
-
-            //now check for ans
-            if(mpp.size()==1)
-            res=max(res,num[i]-'0');
+            if(num[i]==num[i+1] && num[i]==num[i+2])
+            {
+                ans=max(ans,num.substr(i,3));
+            }
         }
-
-
-        if(res==-1) return ans;
-
-        ans+=to_string(res)+to_string(res)+to_string(res);
-
         return ans;
     }
 };
