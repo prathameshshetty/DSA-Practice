ec6c21a92c89378e05148d9e0b1ceaf9a60759bf
Prathamesh Shetty <145214206+prathameshshetty@users.noreply.github.com>
2025-08-21 03:49:36 +0530

    Time: 11 ms (28.49%) | Memory: 30.5 MB (21.42%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
@@ -1,40 +1,45 @@
 class Solution {
 public:
-    int solve(int i,int j,vector<vector<int>>&matrix,vector<vector<int>>& dp)
-    {
-        int m=matrix.size();
-        int n=matrix[0].size();
-        if(i>=m || j>=n) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(matrix[i][j]==0)  return 0;
-
-        int right=solve(i,j+1,matrix,dp);
-        int diag=solve(i+1,j+1,matrix,dp);
-        int below=solve(i+1,j,matrix,dp);
-
-        return dp[i][j]=1+min({right,diag,below});
-    }
-
-
-
     int countSquares(vector<vector<int>>& matrix) {
         int m=matrix.size();
         int n=matrix[0].size();
-        int res=0;
 
-        vector<vector<int>> dp(m,vector<int>(n,-1));
+        int ans=0;
+
+        vector<vector<int>> dp(m,(vector<int>(n,0)));
+
+        //first row and first col must be filled as it is
+
+        //for first col
         for(int i=0;i<m;i++)
         {
-            for(int j=0;j<n;j++)
-            {
-                if(matrix[i][j]==1)
-                {
-                    res+=solve(i,j,matrix,dp);
-                }
-            }
+            dp[i][0]=matrix[i][0];
+            ans+=matrix[i][0];
+        }
+
+        //first row
+        for(int i=1;i<n;i++)
+        {
+            dp[0][i]=matrix[0][i];
+            ans+=matrix[0][i];
         }
 
 
-        return res;
+
+        for(int i=1;i<m;i++)
+        {
+            for(int j=1;j<n;j++)
+            {
+                if(matrix[i][j]==1)
+                {
+                    dp[i][j]=1+min({dp[i-1][j],dp[i-1][j-1],dp[i][j-1]});
+                    ans+=dp[i][j];
+                }
+                else dp[i][j]=0;
+            }
+        }
+
+        return ans;
+
     }
 };
