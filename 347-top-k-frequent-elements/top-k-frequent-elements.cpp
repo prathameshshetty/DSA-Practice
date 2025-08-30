a2b4c7747b969309d01b5fae442aeb40878c88a7
Prathamesh Shetty <145214206+prathameshshetty@users.noreply.github.com>
2025-08-30 19:20:51 +0530

    Time: 7 ms (15.90%) | Memory: 18 MB (34.86%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -2,19 +2,16 @@ class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         unordered_map<int,int> mpp;
-
-        for(auto it:nums)
+        int n=nums.size();
+        for(int i=0;i<n;i++)
         {
-            mpp[it]++;
+            mpp[nums[i]]++;
         }
 
-        //CAn i put sort to map?
-        //Nope
-        //I will use a vector of paris
-        //Else i can directly use priority queue
         vector<pair<int,int>> vec(mpp.begin(),mpp.end());
 
-        sort(vec.begin(),vec.end(),[](pair<int,int> a,pair<int,int> b){
+        sort(vec.begin(),vec.end(),[](pair<int,int> a,pair<int,int> b)
+        {
             return a.second>b.second;
         });
 
@@ -22,10 +19,10 @@ public:
 
         for(int i=0;i<k;i++)
         {
-            int temp=vec[i].first;
-            ans.push_back(temp);
+            ans.push_back(vec[i].first);
         }
 
         return ans;
     }
+
 };
