bf687f3d683ce3ef36a64f57b34b6e4b1c7ea232
Prathamesh Shetty <145214206+prathameshshetty@users.noreply.github.com>
2025-08-30 08:23:48 +0530

    Time: 1 ms (57.76%) | Memory: 23.5 MB (95.01%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         int n=nums.size();
-
         int cnt1=0,cnt2=0;
+
         int ele1=INT_MIN,ele2=INT_MAX;
 
         for(int i=0;i<n;i++)
@@ -18,37 +18,27 @@ public:
                 cnt2=1;
                 ele2=nums[i];
             }
-            else if(nums[i]==ele1)
-            {
-                cnt1++;
-               
-            }         
-            else if(nums[i]==ele2)
-            {
-               
-                cnt2++;
-            }
+            else if(ele1==nums[i]) cnt1++;
+            else if(ele2==nums[i]) cnt2++;
             else{
                 cnt1--;
                 cnt2--;
             }
         }
 
-        //Now I goota veriify
-        int cntEle1=0,cntEle2=0;
-        for(int i=0;i<n;i++)
-        {
-            if(ele1==nums[i]) cntEle1++;
-            if(ele2==nums[i]) cntEle2++;
-        }
-
         vector<int> ans;
 
-        if(cntEle1> (n/3)) ans.push_back(ele1);
-        if(cntEle2 > (n/3)) ans.push_back(ele2);
+        int cnt11=0,cnt21=0;
+
+        for(int i=0;i<n;i++)
+        {
+            if(ele1==nums[i]) cnt11++;
+            if(ele2==nums[i]) cnt21++;
+        }
+
+        if(cnt11>(n/3)) ans.push_back(ele1);
+        if(cnt21>(n/3)) ans.push_back(ele2);
 
         return ans;
-        
-
     }
 };
