52ae4f1ab1571250f0ab52d40bb44a099bced061
Prathamesh Shetty <145214206+prathameshshetty@users.noreply.github.com>
2025-08-30 09:27:13 +0530

    Time: 51 ms (50.30%) | Memory: 30.9 MB (32.10%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,14 +1,10 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        //Here using two pointers inside a for loop
-        //checking for duplicates at 2 places to avoid duplicate triplets
-        
-
-        vector<vector<int>> ans;
-
         sort(nums.begin(),nums.end());
         int n=nums.size();
+
+        vector<vector<int>> ans;
         for(int i=0;i<n;i++)
         {
             if(i>0 && nums[i]==nums[i-1]) continue;
@@ -23,19 +19,19 @@ public:
                 if(sum==0)
                 {
                     vector<int> temp={nums[i],nums[low],nums[high]};
-
-                    ans.push_back(temp);
                     low++;
                     high--;
 
+                    ans.push_back(temp);
+                    //now to avoid duplicates
                     while(low<high && nums[low]==nums[low-1]) low++;
                     while(low<high && nums[high]==nums[high+1]) high--;
                 }
                 else if(sum>0) high--;
                 else low++;
             }
-    }
+        }
 
-    return ans;
+        return ans;
     }
 };
