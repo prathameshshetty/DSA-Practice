50e2b06bd38068891beff3acc9b180a9119a2fbe
Prathamesh Shetty <145214206+prathameshshetty@users.noreply.github.com>
2026-04-29 13:49:16 +0530

    Time: 22 ms (30.11%) | Memory: 80.4 MB (77.98%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,13 @@
+import java.util.HashSet;
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> s=new HashSet<>();
+        int n = nums.length;
+        for(int i=0;i<n;i++){
+            s.add(nums[i]);
+        }
+       if (s.size()== n)
+       return false;
+        return true;
+    }
+}
