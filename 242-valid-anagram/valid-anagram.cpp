3c0bea726624d01b29019871114f840a11ed1d58
Prathamesh Shetty <145214206+prathameshshetty@users.noreply.github.com>
2026-06-11 15:05:15 +0530

    Time: 7 ms (19.16%) | Memory: 9.5 MB (92.10%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,27 +1,11 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        int n=s.size();
-        int m=t.size();
-        if(m!=n) return false;
-
-        unordered_map<char,int> mpp;
-
-        for(int i=0;i<n;i++)
-        {
-            mpp[s[i]]++;
+        if (s.length()!= t.length()){
+            return false;
         }
-
-        //now remove
-        for(int i=0;i<m;i++)
-        {
-            mpp[t[i]]--;
-            
-        }
-        
-        for(auto it:mpp) if(it.second!=0) return false;
-        
-        //now check anythin gis left
-return true;
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        return s==t;
     }
 };
