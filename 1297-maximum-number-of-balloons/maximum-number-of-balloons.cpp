1897dc7333adb7b87c6e2c275ebe26ca7ea19a79
Prathamesh Shetty <145214206+prathameshshetty@users.noreply.github.com>
2026-06-22 13:37:44 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (84.03%) - LeetSync

---
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maxNumberOfBalloons(string text) {
+        int freq[26]={0};
+
+        for(char ch: text) freq[ch-'a']++;
+
+
+        return min({freq[0],freq[1],freq['l'-'a']>>1,freq['o'-'a']>>1,freq['n'-'a']});
+    }
+};
