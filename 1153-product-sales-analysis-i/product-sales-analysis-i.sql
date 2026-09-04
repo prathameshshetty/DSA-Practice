7415e7c57ac4a09afaf6f40b0e9423e7d9859e8f
Prathamesh Shetty <145214206+prathameshshetty@users.noreply.github.com>
2026-09-05 01:09:51 +0530

    Time: 2604 ms (90.95%) | Memory: 0B (100.00%) - LeetSync

--- 1153-product-sales-analysis-i/product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/product-sales-analysis-i.sql
@@ -1,5 +1,5 @@
-# Write your MySQL query statement below
-SELECT Product.product_name,Sales.year,Sales.price
-FROM Sales
-JOIN Product
-ON Sales.product_id=Product.product_id;
+/* Write your T-SQL query statement below */
+SELECT p.product_name,s.year,s.price
+FROM Sales s
+INNER JOIN Product p
+ON s.product_id= p.product_id;
